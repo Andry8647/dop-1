01d05682d5808f44667dca439b2418703386aa88
Андрей <honor10love2@gmail.com>
2023-03-05 14:52:41 +0300

    Задача на функциональное програмированние

--- src/Main.java
+++ src/Main.java
@@ -1,24 +1,25 @@
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Scanner;
+import java.util.*;
+import java.util.Map.Entry;
 
 public class Main {
+
     public static void main(String[] args) {
-        try {
-            Scanner scanner = new Scanner(System.in);
+//        try {
+//            Scanner scanner = new Scanner(System.in);
+//
+//            System.out.println("Введите число:");
+//            int d = scanner.nextInt();
+//            System.out.println("Введите 1 или 0");
+//            int b = scanner.nextInt();
+//            if (b != 0 & b!=1){
+//                throw new UnsupportedOperationException("Число неправильное Введите 1 или 0");
+//            }
+//
+//            System.out.println(d/b);
+//        }catch (ArithmeticException e){
+//            System.out.println("На ноль делить нельзя");
+//        }
 
-            System.out.println("Введите число:");
-            int d = scanner.nextInt();
-            System.out.println("Введите 1 или 0");
-            int b = scanner.nextInt();
-            if (b != 0 & b!=1){
-                throw new UnsupportedOperationException("Число неправильное Введите 1 или 0");
-            }
-
-            System.out.println(d/b);
-        }catch (ArithmeticException e){
-            System.out.println("На ноль делить нельзя");
-        }
 
 
         Scanner scanner = new Scanner(System.in);
@@ -43,11 +44,20 @@ public class Main {
                 repetition.put(d, 1);
             }
         }
+
         System.out.println("ТОП 10:");
         int t = 0;
-        for (Map.Entry<String, Integer> a : repetition.entrySet()) {
-            System.out.println(a.getKey() + " - " + a.getValue());
+        int i = 0;
+        for (Entry<String, Integer> a : repetition.entrySet()) {
+            Map<String, Integer> sortedTreeMap = new TreeMap<>(Comparator.reverseOrder());
+            sortedTreeMap.put(a.getKey(),a.getValue());
+            if (i < 10){
+                System.out.println(sortedTreeMap);
             t = a.getValue();
+            i++;
+           }
         }
+
+
     }
 }
